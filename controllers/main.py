929fe4e45d4497211bc400b610db8b87c720a024
JyothiDuggirala <jyothidkiran@gmail.com>
2022-01-23 21:59:30 +0100

    imported controllers, added form view

--- controllers/main.py
+++ controllers/main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import datetime
 import logging
-
 from odoo import http
 
 _logger = logging.getLogger(__name__)
@@ -15,6 +15,7 @@ class RainRecorder(http.Controller):
         _logger.info('saving rain measuerment')
         # assert height is not null, "Incorrect rating"
         obj = http.request.env['rain.measurement'].create({
+            'date': datetime.now,
             'rainQuantity': height,
         })
         return obj
--- models/rain_measurement.py
+++ models/rain_measurement.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from email.policy import default
 from odoo import fields,models
 
@@ -6,6 +5,6 @@ class RainMeasurement(models.Model):
     _name = "rain.measurement"
     _description = "contains historical data of rain"
 
-    # id = fields.Char("unique id",required=True)
-    date = fields.Datetime("rain measurement date",required=True, default=datetime.now)
+    id = fields.Char("unique id",required=True)
+    date = fields.Datetime("rain measurement date",required=True)
     rainQuantity = fields.Integer("rain measurment in mm",required=True)
